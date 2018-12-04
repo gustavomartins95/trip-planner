5412eb24b42bfbc7c7722dbd103756488f9b11ba
Gustavo Martins <gustavo_sh8@hotmail.com>
2018-12-03 22:35:18 -0200

    First Commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import HomeScreen from './src/screens/home-screen';
+
+export default class App extends React.Component {
+  render() {
+    return <HomeScreen />;
+  }
+}
---
+++ src/screens/home-screen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import { View, Text } from 'react-native';
+
+export default class HomeScreen extends Component {
+  render() {
+    return (
+      <View style={{ paddingTop: 30 }}>
+        <Text>Olá Hands-on RN!</Text>
+      </View>);
+  }
+}
