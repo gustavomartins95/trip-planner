800d29bc05ebe77d65e9d32f8cecb64769e1ec78
Gustavo Martins <gustavo_sh8@hotmail.com>
2018-12-08 18:55:38 -0200

    Trips Screen

---
+++ src/screens/TripsScreen/Trip/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+
+export default styles = StyleSheet.create({
+  wrapperTrip: {
+    backgroundColor: '#FFFFFF',
+    paddingTop: 14,
+    paddingLeft: 14,
+    paddingRight: 14,
+  },
+  image: {
+    backgroundColor: '#EEEEEE',
+    height: 198,
+    marginBottom: 6,
+  },
+  price: {
+    position: 'absolute',
+    top: 198 - 44,
+    right: 34,
+    textAlign: 'right',
+    backgroundColor: '#24C6DC',
+    paddingTop: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+    paddingLeft: 10,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  title: {
+    paddingBottom: 6,
+  }
+})
--- src/screens/TripsScreen/index.js
+++ src/screens/TripsScreen/index.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
 
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 
-export default class HomeScreen extends Component {
+import Trip from './Trip';
+
+export default class TripsScreen extends Component {
+
+  static navigationOptions = {
+    header: null
+  }
+
+  renderItem = item => {
+    return <Trip title={item.item.name} price={item.item.price} />
+  }
+
   render() {
+    const trips = [
+      { id: '1', name: 'Eurotrip 2018', price: 'R$5000' },
+      { id: '2', name: 'Expedição Atacama', price: 'R$3500' },
+    ]
     return (
-      <Text>Trips Screen</Text>
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'stretch',
+      }}>
+        <View style={{
+          backgroundColor: '#DDDDDD',
+          flex: 1,
+        }}>
+          <Text style={{ paddingTop: 25 }}>Mapa</Text>
+        </View>
+        <View>
+          <FlatList
+            data={trips}
+            renderItem={this.renderItem}
+            keyExtractor={item => item.id}
+            horizontal
+            pagingEnabled
+          />
+        </View>
+      </View>
     );
   }
 }
