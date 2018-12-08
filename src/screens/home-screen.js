7bf3132cde3d47a1b0eddd3818a9e8388a79a033
Gustavo Martins <gustavo_sh8@hotmail.com>
2018-12-08 14:03:53 -0200

    StyleSheet

--- src/screens/home-screen.js
+++ src/screens/home-screen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { View, Text, ImageBackground, Image, TouchableWithoutFeedback } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, Image, TouchableWithoutFeedback } from 'react-native';
 
 export default class HomeScreen extends Component {
   render() {
@@ -8,38 +8,49 @@ export default class HomeScreen extends Component {
       <ImageBackground
         source={require('../../assets/background.png')}
         imageStyle={{ resizeMode: 'stretch' }}
-        style={{
-          flex: 1,
-          flexDirection: 'column',
-          alignItems: 'stretch',
-          justifyContent: 'space-between',
-        }}
+        style={styles.background}
       >
-        <View style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
+        <View style={styles.wrapperLogoTripPlanner}>
           <Image source={require('../../assets/logo-tripplanner.png')} />
         </View>
-        <View style={{
-          flexDirection: 'row',
-          alignItems: 'flex-end',
-          justifyContent: 'center',
-          paddingBottom: 32,
-        }}>
+        <View style={styles.wrapperLogoDevPleno}>
           <Image source={require('../../assets/logo-devpleno.png')} />
         </View>
         <TouchableWithoutFeedback>
-          <View style={{
-            backgroundColor: '#fff',
-            paddingTop: 16,
-            paddingBottom: 16,
-          }}>
-            <Text style={{ textAlign: 'center', fontSize: 18 }}>COMEÇAR</Text>
+          <View style={styles.buttonBackground}>
+            <Text style={styles.buttonText}>COMEÇAR</Text>
           </View>
         </TouchableWithoutFeedback>
       </ImageBackground>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    justifyContent: 'space-between',
+  },
+  wrapperLogoTripPlanner: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  wrapperLogoDevPleno: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    paddingBottom: 32,
+  },
+  buttonBackground: {
+    backgroundColor: '#fff',
+    paddingTop: 16,
+    paddingBottom: 16,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 18,
+  }
+})
